ca2e51d59efd0329bcf459df9a1270adfc9f8b08
Peter Holm <peter@l-holm.dk>
2021-05-03 20:43:11 +0200

    unix version

--- compute/wsgi.py
+++ compute/wsgi.py
@@ -14,5 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compute.settings')
 
 os.environ['MPLCONFIGDIR'] = '/tmp/'
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 application = get_wsgi_application()
--- setup/createwebsite.sh
+++ setup/createwebsite.sh
@@ -7,8 +7,10 @@
 
 sudo apt install libapache2-mod-wsgi-py3
 
-sudo ln -s /var/www/danbots/compute/apache/danbot-compute.conf /etc/apache2/sites-available/danbots-compute.conf
+sudo ln -s /var/www/danbots/compute/apache/danbots-compute.conf /etc/apache2/sites-available/danbots-compute.conf
 sudo a2ensite danbots-compute.conf
 
-sudo mkdir /var/www/danbots/compute/data
+sudo mkdir /var/log/apache2/danbots
+sudo mkdir /var/www/danbots/compute/data /var/www/danbots/compute/site
+
 sudo chown www-data /var/www/danbots/compute/data
