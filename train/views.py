dbd503a1aa8ed0a24572acb7f58085fe95441ab7
Peter Holm <peter@l-holm.dk>
2021-09-05 11:21:44 +0200

    show pic

--- train/views.py
+++ train/views.py
@@ -9,14 +9,14 @@ def showresult(request):
     next_val = int(request.GET.get('next',0))
     next_index = folder_index + next_val
     #print ("Index: ", index, " Next: ", next)
-    picpath = '/data/device/b827eb841738/input/' + str(next_index) + '/'
+    picpath = '/data/device//dca6320b6bd5/input/' + str(next_index) + '/'
 
     mycontext = {
         'index': next_index,
         'path': picpath,
-        'pic1': picpath + "image0.png",
-        'pic2': picpath + "image8.png",
-        'pic3': picpath + "image9.png",
+        'pic1': picpath + "image8.jpg",
+        'pic2': picpath + "image0.jpg",
+        'pic3': picpath + "image9.jpg",
         'pic4': picpath + "mask.png",
         'pic5': picpath + "nndepth.png",
         'pic6': picpath + "nnkdata.png",
