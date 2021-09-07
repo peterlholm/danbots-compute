dd7c4e4a9396101855396ae46212478d3aca3db1
Peter Holm <peter@l-holm.dk>
2021-09-07 08:01:45 +0200

    update

--- train/views.py
+++ train/views.py
@@ -9,7 +9,9 @@ def showresult(request):
     next_val = int(request.GET.get('next',0))
     next_index = folder_index + next_val
     #print ("Index: ", index, " Next: ", next)
-    picpath = '/data/device//dca6320b6bd5/input/' + str(next_index) + '/'
+    #picpath = '/data/device//dca6320b6bd5/input/' + str(next_index) + '/'
+    #picpath = '/data/device//b827eb841738/input/' + str(next_index) + '/'
+    picpath = '/data/device//b827eb05abc2/input/' + str(next_index) + '/'
 
     mycontext = {
         'index': next_index,
