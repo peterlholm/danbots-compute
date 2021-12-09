c96c34acd0049ccb2ac430e8f5e0d8d1c0f93363
peterlholm <peter@l-holm.dk>
2021-12-09 10:32:25 +0100

    Update wsgi.py

--- compute/wsgi.py
+++ compute/wsgi.py
@@ -14,7 +14,15 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compute.settings')
 
 os.environ['MPLCONFIGDIR'] = '/tmp/'
+
+"""
+TF_CPP_MIN_LOG_LEVEL - which has 3 or 4 basic levels - low numbers = more messages.
+    0 outputs Information, Warning, Error, and Fatals (default)
+    1 outputs Warning, and above
+    2 outputs Errors and above.
+"""
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
 #os.environ["DISPLAY"] = "0.0"
 
 application = get_wsgi_application()
