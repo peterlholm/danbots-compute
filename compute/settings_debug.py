db9dcad18f4cb45016a28193ef3e22568857b526
Peter Holm <peter@danbots.com>
2022-03-29 15:34:16 +0200

    update

--- compute/settings_debug.py
+++ compute/settings_debug.py
@@ -4,8 +4,8 @@
 DEBUG = True
 
 NN_ENABLE=False
+
 #MYDEVICE = "b827eb05abc2"
-print("Settings.debug")
 #print('NN_ENABLE', NN_ENABLE)
 
 LOGGING = {
--- setup/createwebsite.sh
+++ setup/createwebsite.sh
@@ -7,11 +7,11 @@
 
 sudo apt install libapache2-mod-wsgi-py3
 
-sudo ln -s /var/www/danbots/compute/apache/danbots-compute.conf /etc/apache2/sites-available/danbots-compute.conf
+sudo ln -s /var/www/compute/apache/danbots-compute.conf /etc/apache2/sites-available/danbots-compute.conf
 sudo a2ensite danbots-compute.conf
 
-sudo mkdir /var/log/apache2/danbots
-sudo mkdir /var/www/danbots/compute/data /var/www/danbots/compute/site
+#sudo mkdir /var/log/apache2/
+sudo mkdir /var/www/compute/data /var/www/compute/site
 
-sudo chown www-data /var/www/danbots/compute/data
-sudo chgrp -R www-data /var/www/danbots/compute
+sudo chown www-data /var/www/compute/data
+sudo chgrp -R www-data /var/www/compute
